f66ae5c49303f528441883bc6e98f84308185c9f
Dima Zavin <dima@android.com>
2010-02-02 15:38:43 -0800

    platform: msm_shared/msm7x30: initialize uart1 for 7x30 correctly
    Signed-off-by: Dima Zavin <dima@android.com>

--- platform/msm_shared/uart.c
+++ platform/msm_shared/uart.c
@@ -119,7 +119,11 @@
 
 
 static unsigned uart_ready = 0;
+#if PLATFORM_MSM7X30
+static unsigned uart_base = MSM_UART1_BASE;
+#else
 static unsigned uart_base = MSM_UART3_BASE;
+#endif
 
 #define uwr(v,a) writel(v, uart_base + (a))
 #define urd(a) readl(uart_base + (a))
@@ -132,7 +136,7 @@ void uart_init(void)
 	uwr(0x10, UART_CR);  /* reset receiver */
 	uwr(0x20, UART_CR);  /* reset transmitter */
 	
-#if PLATFORM_QSD8K
+#if PLATFORM_QSD8K || PLATFORM_MSM7X30
 	/* TCXO */
 	uwr(0x06, UART_MREG);
 	uwr(0xF1, UART_NREG);
