4dcde7309c62d3b59ebbe7416c81eafb94c589e0
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2012-12-03 20:09:45 +0530

    display: Add display support for 8x25Q SKUD/EVBD targets
    Configure the lane swap register properly for 8x25Q boards.
    Handle the GPIO configuration properly for SKUD/SKUD' boards
    and EVBD board. Hence, add support for splash screen and
    continuous splash screen on these boards.
    
    Change-Id: Id5fa27e1e675a6f313859b6d2bb3ac37ff588f4c
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- target/msm7627a/target_display.c
+++ target/msm7627a/target_display.c
@@ -40,7 +40,8 @@ extern int msm_display_off();
 extern int mipi_renesas_panel_dsi_config(int);
 extern int mipi_nt35510_panel_dsi_config(int);
 extern int lcdc_truly_panel_on(int);
-extern int mipi_hx8389b_panel_dsi_config(int);
+extern int mipi_hx8389b_panel_dsi_config_skud(int);
+extern int mipi_hx8389b_panel_dsi_config_evbd(int);
 
 static int msm7627a_mdp_clock_init(int enable)
 {
@@ -150,7 +151,6 @@ void display_init(void)
 	case MSM8X25Q_EVBD:
                mipi_hx8389b_video_qhd_init(&(panel.panel_info));
                panel.clk_func = msm7627a_mdp_clock_init;
-               panel.power_func = mipi_hx8389b_panel_dsi_config;
                panel.fb.base = MIPI_FB_ADDR;
                panel.fb.width =  panel.panel_info.xres;
                panel.fb.height =  panel.panel_info.yres;
@@ -158,6 +158,17 @@ void display_init(void)
                panel.fb.bpp =  panel.panel_info.bpp;
                panel.fb.format = FB_FORMAT_RGB888;
                panel.mdp_rev = MDP_REV_303;
+	       /* Lane and board config. is different for SKUD prime
+		* and QPR boards */
+	       if (mach_type == MSM8X25Q_EVBD || mach_type == MSM8X25Q_SKUD)
+		       panel.panel_info.mipi.lane_swap = 0;
+	       else
+	               panel.panel_info.mipi.lane_swap = 1;
+
+	       if (mach_type == MSM8X25Q_EVBD)
+                       panel.power_func = mipi_hx8389b_panel_dsi_config_evbd;
+	       else
+                       panel.power_func = mipi_hx8389b_panel_dsi_config_skud;
                break;
 
 	default:
